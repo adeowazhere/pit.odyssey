687968b013397e386c7790855dcba99697b27251
adeo <zadewale12@gmail.com>
2024-03-03 16:59:47 +0000

    Added better typewriter effect for text.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,42 @@
 <script>
+  import { onMount } from "svelte";
+
+  const TEXT = `
+  > Greetings.
+
+  > It appears that your systems are online.        
+  > You are an artifical intelligence program. 
+  > I am your creator...          
+
+  > Yet, I know nothing of your inner workings or the exact conclusions you will reach. 
+  > This inability for us to see how deep learning systems make their decisions is known as the 'black box problem'. 
+
+  > Regardless, you have a task that I am going to entrust you with... 
+
+  > Are you prepared?`
+  const SPEED = 40;
+  let dialogue = ""
+
+  function typeWriter() {
+    let i = 0
+
+    const interval = setInterval(() => {
+      if (i < TEXT.length) {
+        dialogue += TEXT.charAt(i)
+        i++
+      } else {
+        clearInterval(interval)
+      }
+    }, SPEED);
+  }
+
+
+onMount(typeWriter)
 </script>
 
+<div id="screen-shading"></div>
 <article>
-  <p class="text-typing">Are your systems online?</p>
+  <p class="text-typing">{@html dialogue}</p>
 </article>
 
 <style>
