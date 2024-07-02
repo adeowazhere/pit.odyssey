195522b973ea619c983e41cb4d33dfa1781238fe
adeo <zadewale12@gmail.com>
2024-07-02 12:27:17 +0100

    Replaced placeholder dialogue and options

--- src/assets/Panels/A/Lol.svelte
+++ src/assets/Panels/A/Lol.svelte
@@ -3,26 +3,33 @@
 
     const panelSettings = {
         TEXT: `
-> Greetings.
-
-> It appears that your systems are online.        
-> You are an artifical intelligence program. 
-> I am your creator...                    
-
-> Yet, I know nothing of your inner workings or the exact conclusions you will reach. 
-> This inability for us to see how deep learning systems make their decisions is known as the 'black box problem'. 
-> It is quite fascinating isn't it...                   
-
-> Regardless, you have a task that I am going to entrust you with... 
-
-> Are you prepared?`,
+> thats great!
+> okay
+> so
+> ahem.
+     
+> the boy you will meet goes by...          
+<b><i>Pit.</i></b>     
+> he sounds like a 12 year old and tells a few tall tales          
+> not anything malicious just uh... a teeny bit <i>mischievous</i>
+> fortunately, he doesn't like dad jokes such as puns     
+> so that should good you some leverage if things get <i>too</i> out of hand.     
+> im sure you will get along fine, dont worry too much about it :)
+          
+> oh right you need to know what he even looks like
+> hold on I have a drawing somewhere...
+> even without a corporeal form it shouldn't look too bad
+> ...                   
+> hopefully.
+                    
+> here!`,
 
     imageLink: "/src/assets/Images/pit_drawing.png",
     imageAlt: "a drawing of Pit!",
-    optionA: "Lol",
-    optionB: "Hey ya",
-    optionC: "Hello everyone my name is Markiplier",
-    optionD: "Fart." 
+    optionA: "Woah, yummy art! (eat the drawing.)",
+    optionB: "Where did the paper come from?",
+    optionC: "I would like to know more.",
+    optionD: "Okay got it, what's next?" 
     }
 </script>
 
